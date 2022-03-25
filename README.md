ff0569336eaa49c3767adda0101aff838a27e609
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2022-03-25 09:12:39 -0500

    create contract for minting nfts

---
+++ README.md
@@ -0,0 +1,43 @@
+# Full Mint Website
+
+(Building a Full Web3 Mint Website for Beginners | React, Hardhat, Ethers, and Chakra
+)[https://www.youtube.com/watch?v=ynFNLBP2TPs]
+
+Steps:
+_Create frontend app_
+`npx create-react-app full-mint-website`
+
+_Install hardhat as a dependency_
+`npm i -D hardhat`
+
+_Create a hardhat project_
+`npx hardhat`
+
+- Create a basic sample project
+- /path/to/project
+- y
+- y
+
+_Install openzeppelin contracts_
+
+> useful for using stardard contract functionality like minting
+> `npm i @openzeppelin/contracts`
+
+_Install chakra for using pre-built styling components_
+`npm i @chakra-ui/react @emotion/react@^11 @emotion/styled@^11 framer-motion@^6`
+
+# Basic Sample Hardhat Project
+
+This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, a sample script that deploys that contract, and an example of a task implementation, which simply lists the available accounts.
+
+Try running some of the following tasks:
+
+```shell
+npx hardhat accounts
+npx hardhat compile
+npx hardhat clean
+npx hardhat test
+npx hardhat node
+node scripts/sample-script.js
+npx hardhat help
+```
---
+++ contracts/RoboPunksNFT.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.4;
+
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+
+// inherit ERC721 and Ownable for some base functionality
+contract RoboPunksNFT is ERC721, Ownable {
+  uint256 public mintPrice;
+  uint256 public totalSupply;
+  uint256 public maxSupply;
+  uint256 public maxPerWallet;
+  bool public isPublicMintEnabled;
+  string internal baseTokenUri; // NFT location
+  address payable public withdrawWallet; // for withdrawing money
+  mapping(address => uint256) public walletMints; // keep track of all mints
+  
+  constructor() payable ERC721('RoboPunks', 'ROBO') {
+    mintPrice = 0.02 ether;
+    totalSupply = 0;
+    maxSupply = 1000;
+    maxPerWallet = 3;
+    // set withdraw wallet address 
+  }
+
+  function setIsPublicMintEnabled(bool isPublicMintEnabled_) external onlyOwner {
+    isPublicMintEnabled = isPublicMintEnabled_;
+  }
+
+  function setBaseTokenUri(string calldata baseTokenUri_) external onlyOwner {
+    baseTokenUri = baseTokenUri_;
+  }
+
+  // ERC721 function used to grab NFTs
+  function tokenURI(uint256 tokenId_) public view override returns (string memory) {
+    require(_exists(tokenId_), 'Token does not exist!');
+
+    return string(abi.encodePacked(baseTokenUri, Strings.toString(tokenId_), ".json"));
+  }
+
+  function withdraw() external onlyOwner {
+    (bool success, ) = withdrawWallet.call{ value: address(this).balance }('');
+    require(success, 'withdraw failed');
+  }
+
+  function mint(uint256 quantity_) public payable {
+    require(isPublicMintEnabled, 'minting not enabled');
+    require(msg.value == quantity_ * mintPrice, 'wrong mint value');
+    require(totalSupply + quantity_ <= maxSupply, 'sold out');
+    require(walletMints[msg.sender] + quantity_ <= maxPerWallet, 'exceeded max wallet amount');
+
+    for (uint256 i = 0; i < quantity_; i++) {
+      uint256 newTokenId = totalSupply + 1;
+      totalSupply++; // effect, should be called before an interaction
+      _safeMint(msg.sender, newTokenId); // interaction
+    }
+  }
+}
